11274841314c196910e2516bcb24a45a16c3ad48
miniity <miniitiy.18@gmail.com>
2023-11-19 22:58:48 +0900

    ユーザー管理機能-修正①

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :items
-  has_many :records
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)\w*\z/ }
+  validates :password, presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)\w*\z/ }
   validates :first_name, presence: true, format: { with: /\A[^ -~｡-ﾟ]+\z/ }
   validates :last_name, presence: true, format: { with: /\A[^ -~｡-ﾟ]+\z/ }
-  validates :first_kananame, presence: true, format: { with: /\A[^ -~｡-ﾟ]+\z/ }
-  validates :last_kananame, presence: true, format: { with: /\A[^ -~｡-ﾟ]+\z/ }
+  validates :first_kananame, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_kananame, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
 end
