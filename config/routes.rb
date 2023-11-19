050a1abf9419a147f8d9fc577a274b509c342633
miniity <miniitiy.18@gmail.com>
2023-11-19 19:31:05 +0900

    ログイン・ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'items/index'
   devise_for :users
+  get 'items/index'
   root to: "items#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
