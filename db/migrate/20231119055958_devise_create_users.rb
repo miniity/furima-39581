6ea6b6b999b52ba76716f0bfaf6a1a7eae18a22d
miniity <miniitiy.18@gmail.com>
2023-11-19 15:08:47 +0900

    テーブルの設計

--- db/migrate/20231119055958_devise_create_users.rb
+++ db/migrate/20231119055958_devise_create_users.rb
@@ -6,6 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name,               null: false, default: ""
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :last_kananame ,     null: false
+      t.string :first_kananame,     null: false
+      t.date :birthday,             null: false
 
       ## Recoverable
       t.string   :reset_password_token
