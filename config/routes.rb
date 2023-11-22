f5677570e9d3e244603588b9ec8e686014fd1c10
miniity <miniitiy.18@gmail.com>
2023-11-22 22:21:18 +0900

    モデル・テーブルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   root to: "items#index"
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :items, only: :index
 end
