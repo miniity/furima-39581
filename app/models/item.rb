993bb7693188d4ba88135775ffa2632822e2bf61
miniity <miniitiy.18@gmail.com>
2023-11-23 10:14:18 +0900

    バリエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,23 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
   belongs_to :shipping_fee
+
+  validates :image, presence: true
+  validates :product_name, presence: true
+  validates :product_description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_day_id, presence: true
+  validates :sales_price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :sales_price, format: { with: /\A\d+\z/ }
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :shipping_day_id, numericality: { other_than: 1 } 
+  validates :shipping_fee_id, numericality: { other_than: 1 } 
+
 end
