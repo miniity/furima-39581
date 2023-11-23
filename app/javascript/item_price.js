00377e9bb55b5b70c364c8736048439da56081b8
miniity <miniitiy.18@gmail.com>
2023-11-23 11:46:07 +0900

    要素を取得

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,11 @@
 window.addEventListener('turbo:load', () => {
   console.log("OK");
 });
+
+// 金額を入力した数値をpriceInputという変数に格納する
+const itemPriceInput = document.getElementById("item-price");
+console.log(itemPriceInput);
+const taxPriceInput = document.getElementById("add-tax-price");
+console.log(taxPriceInput);
+const profitInput = document.getElementById("profit");
+console.log(profitInput);
