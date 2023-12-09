e5063dbe712ea6e613af85c06d67c06583ce1ced
miniity <miniitiy.18@gmail.com>
2023-12-09 12:06:06 +0900

    最終課題-修正②

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,41 +3,35 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :check_seller, only: [:index, :create]
 
+
   def index
-    setup_order_form
-  end
-
-  def create
-    @item = Item.find(params[:item_id])
-    @order_address = OrderAddress.new(order_params)
-
-    if @order_address.valid?
-      process_payment
-      @order_address.save
-      redirect_to root_path
-    else
-      setup_order_form
-      render :index, status: :unprocessable_entity
-    end
-  end
-
-  private
-
-  def setup_order_form
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
   end
 
-  def process_payment
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    Payjp::Charge.create(
-      amount: @item.sales_price,  
-      card: order_params[:token], 
-      currency: 'jpy'   
-    )
-  end
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+  
+    if @order_address.valid?
 
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.sales_price,  
+        card: order_params[:token], 
+        currency: 'jpy'   
+      )
+
+      @order_address.save
+      redirect_to root_path
+    else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      render :index, status: :unprocessable_entity
+    end
+  end
+  
+  private
   def check_seller
     item = Item.find(params[:item_id])
 
@@ -48,6 +42,9 @@ class OrdersController < ApplicationController
 
     if current_user == item.user || item.sold_out?
       redirect_to root_path
+      return
+    else
+      return true
     end
   end
 
